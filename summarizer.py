73cd7db0e4e92c1c490de5efcb55f0a8de06c8b2
Sudarshan Parthasarathy <omegacore123@yahoo.in>
2020-07-03 17:49:06 +0530

    add build sentence_similarity function

--- summarizer.py
+++ summarizer.py
@@ -15,6 +15,30 @@ def read_article(filename):
 
 	return sentences
 
+def sentence_similarity(sent1,sent2, stopwords = None):
+	if stopwords is None:
+		stopwords = []
+
+	sent1 = [w.lower() for w in sent1]
+	sent2 = [w.lower() for w in sent2]
+
+	all_words = list(set(sent1 + sent2))
+
+	vector1 = [0] * len(all_words)
+	vector2 = [0] * len(all_words)
+
+	#vector for first sentence
+	for w in sent1:
+		if w in stopwords:
+			continue
+		vector1[all_words.index(w)] += 1
+    for w in sent2:
+		if w in stopwords:
+			continue
+		vector2[all_words.index(w)] += 1
+
+	return 1 - cosine_distance(vector1,vector2)
+
 def build_similarity_matrix(sentences,stop_words):
 	similarity_matrix = np.zeroes(len(sentences),len(sentences))
 	for i1 in range(len(sentences)):
@@ -24,3 +48,4 @@ def build_similarity_matrix(sentences,stop_words):
 			similarity_matrix[i1][i2] = sentence_similarity(sentences[i1],sentences[i2],stop_words)
     
     return similarity_matrix
+
